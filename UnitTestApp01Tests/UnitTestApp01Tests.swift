442c1d8fb4c18784aff32fce829c6e1692baf8ef
kubota-yosuke <i.h.s.y.1491@gmail.com>
2023-09-21 15:55:33 +0900

    test3

--- UnitTestApp01Tests/UnitTestApp01Tests.swift
+++ UnitTestApp01Tests/UnitTestApp01Tests.swift
@@ -20,6 +20,7 @@ final class UnitTestApp01Tests: XCTestCase {
     
     //test
     //test#2
+    //test#3
     func test_succeed() {
         let calculationNumber = CalculationNumber()   // 足し算を行うためにインスタンス化
         let answer = calculationNumber.Add(a: 10, b: 20)   // 足し算, answer = 30
